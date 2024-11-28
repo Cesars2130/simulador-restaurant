cf9f29a79a1bb28a195767f30e0ba93c6e47df48
Cesars2130 <Cesars.213008@gmail.com>
2024-11-27 18:02:38 -0600

    maven inicializado

---
+++ restaurant-concurrente/src/main/java/com/concurrente/restaurant/App.java
@@ -0,0 +1,10 @@
+package com.concurrente.restaurant;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
